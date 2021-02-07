cb4bb52ce5f93e8f9c86c7a308809ce929d36a24
Adrian <adrianpros27@gmail.com>
2021-02-07 22:09:42 +0700

    Task 1001

---
+++ src/com/onjug/beginner/Basic.java
@@ -0,0 +1,15 @@
+package com.onjug.beginner;
+
+import java.util.Scanner;
+
+public class Basic {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println("X = " + (A + B ));
+    }
+}
